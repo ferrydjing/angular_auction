be28af2d15aeace9fe888a9d48ec85cbf5f83754
ferrydjing <ferrydjing@outlook.com>
2019-01-09 21:06:52 +0800

    add aution base framware

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ProductsComponent } from './components/products/products.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { StarsComponent } from './components/stars/stars.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    HomeComponent,
+    CarouselComponent,
+    ProductsComponent,
+    FooterComponent,
+    StarsComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule
